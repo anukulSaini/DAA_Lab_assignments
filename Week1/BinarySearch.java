35a44d35813cd01856409e94902f028d8d4e5a34
anukulSaini <anukulsaini2295@gmail.com>
2021-07-25 14:11:16 +0530

    Binary Search

--- Week1/BinarySearch.java
+++ Week1/BinarySearch.java
@@ -1,10 +1,38 @@
 package Week1;
+import java.util.*;
 
 public class BinarySearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array size --> ");
+		int n = sc.nextInt();
+		System.out.println("Enter "+n+ " sorted elements  --> ");
+		int[] arr= new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("no.? --> ");
+		int s= sc.nextInt();
+		int ans = binarysearch(arr,0,n-1,s);
+		if(ans==-1) 
+			System.out.println("Not Present");
+		else
+		System.out.println("Present at "+ans);
 
 	}
+	 static int binarysearch(int[] nums,int l,int r,int target){
+	        if(l<=r){
+	            int mid=(l+r)/2;
+	            if(nums[mid]==target) return mid;
+	            else if(nums[mid]>target)
+	              return  binarysearch(nums,l,mid-1,target);
+	            else
+	               return binarysearch(nums,mid+1,r,target);
+	        }
+	        return -1;
+	    }
+
 
 }
